30938e164ff31f435faa0378edc14f3cef5ce748
rachealynn51 <rachealmayer@Racheals-MacBook-Air.local>
2020-07-24 11:40:32 -0500

    added build

---
+++ client/build/precache-manifest.c6c4f3b1e5c79e6c55be9427573933fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f3b8a4175066238a346195760583a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bd2a4457fd4ca5d58d6",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5c7d535d3ae3fb382cac",
+    "url": "/static/css/main.3e61047d.chunk.css"
+  },
+  {
+    "revision": "9bd2a4457fd4ca5d58d6",
+    "url": "/static/js/2.0f49c905.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f49c905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c7d535d3ae3fb382cac",
+    "url": "/static/js/main.ffec042d.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
